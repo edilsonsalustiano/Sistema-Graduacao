e68253d8bae3986727a9346647e3a29fb4433cbe
Edilson Lima <edilsonlima19777@hotmail.com>
2026-06-03 16:55:30 -0300

    fix: exibe mensagem para credenciais incorretas

--- sistema_graduacao/login/views.py
+++ sistema_graduacao/login/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def login_view(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        print("Usuário:", username)
+        print("Senha:", password)
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        print("Resultado:", user)
+
+        if user is not None:
+            login(request, user)
+            return redirect('inicio')
+
+        messages.error(request, 'Usuário ou senha incorretos.')
+
+    return render(request, 'login/login.html')
